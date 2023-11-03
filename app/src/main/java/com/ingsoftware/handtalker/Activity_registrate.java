b959edde84f4686356f0409ec06665d50b54633b
Tonny <tonny54v@gmail.com>
2023-11-03 10:50:52 -0600

    Ultimos Cambios realizados a la(s) 10:50pm el 03/11/2023

--- app/src/main/java/com/ingsoftware/handtalker/Activity_registrate.java
+++ app/src/main/java/com/ingsoftware/handtalker/Activity_registrate.java
@@ -7,14 +7,33 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.ingsoftware.handtalker.R;
 
-public class Activity_registrate extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Activity_registrate extends AppCompatActivity{
+
+    EditText e1,e2,e3,e4,e5;
+    RequestQueue requestQueue;
+
+    private static final String URL1 = "http://192.168.8.11:8080/handtalker/save.php";
 
     private Button registrarte;
     private ImageView cerrar;
@@ -23,6 +42,14 @@ public class Activity_registrate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrate);
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        e1=(EditText)findViewById(R.id.nombre1);
+        e2=(EditText)findViewById(R.id.apellido1);
+        e3=(EditText)findViewById(R.id.telefono1);
+        e4=(EditText)findViewById(R.id.correo1);
+        e5=(EditText)findViewById(R.id.contrasena1);
 
         // Cambiar el color de la barra de estado
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -45,6 +72,7 @@ public class Activity_registrate extends AppCompatActivity {
         registrarte.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                validar(v);
                 abrirLogin();
             }
         });
@@ -55,4 +83,50 @@ public class Activity_registrate extends AppCompatActivity {
         Intent intent = new Intent(Activity_registrate.this, LoginActivity.class);
         startActivity(intent);
     }
+
+    public void validar(View v){
+        final String nombre = e1.getText().toString();
+        final String apellido =e2.getText().toString();
+        final String telefono =e3.getText().toString();
+        final String correo = e4.getText().toString();
+        final String contrasena =e5.getText().toString();
+
+        createUser(nombre,apellido,telefono,correo,contrasena);
+    }
+
+    private void createUser(String nombre, String apellido, String telefono, String correo, String contrasena) {
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                URL1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(Activity_registrate.this,"Se registró correctamente", Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Activity_registrate.this,"Error del sistema", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("nombre", nombre);
+                params.put("apellido", apellido);
+                params.put("telefono", telefono);
+                params.put("correo", correo);
+                params.put("contrasena", contrasena);
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+    }
+
+
 }
--- app/src/main/java/com/ingsoftware/handtalker/MainActivity.java
+++ app/src/main/java/com/ingsoftware/handtalker/MainActivity.java
@@ -4,11 +4,67 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1,e2,e3,e4,e5;
+    Button registrar1;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        relacionarVistas();
+    }
+
+    public void relacionarVistas(){
+        e1=(EditText)findViewById(R.id.nombre1);
+        e2=(EditText)findViewById(R.id.apellido1);
+        e3=(EditText)findViewById(R.id.telefono1);
+        e4=(EditText)findViewById(R.id.correo1);
+        e5=(EditText)findViewById(R.id.contrasena1);
+        registrar1=(Button)findViewById(R.id.botonRegistra);
+    }
+
+    public void validar(View v){
+        final String nombre = e1.getText().toString();
+        final String Apellido =e2.getText().toString();
+        final String Telefono =e3.getText().toString();
+        final String Correo = e4.getText().toString();
+        final String contrasena =e5.getText().toString();
+
+        String url ="http://localhost:8080/insertarUsuario.php?nombre=" +nombre+ "&Apellido" + Apellido+ "&Telefono" + Telefono + "$Correo" + Correo + "&Contrasena" + contrasena;
+        RequestQueue servicio = Volley.newRequestQueue(this);
+        StringRequest respuesta = new StringRequest(
+                Request.Method.GET, url,
+                response -> {
+                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                },
+                error -> {
+                    Toast.makeText(getApplicationContext(), "Error comunicación", Toast.LENGTH_SHORT).show();
+                }
+        );
+        servicio.add(respuesta);
+
+
     }
 }
